a4c1b88d2938dae764b85d589b7815c4ff7d58d8
Taterao30 <tateraog30@gmail.com>
2026-06-04 16:58:54 +0530

    updated variable file

--- VariablesinJS.js
+++ VariablesinJS.js
@@ -2,3 +2,18 @@ let age=25;
 age=30;
 console.log(age);
 
+age = 35;
+
+console.log(age);
+
+/**
+ * 
+ * | Feature            | `var`          | `let`       | `const`     |
+| ------------------ | -------------- | ----------- | ----------- |
+| Can be reassigned? | ✅ Yes          | ✅ Yes       | ❌ No        |
+| Can be redeclared? | ✅ Yes          | ❌ No        | ❌ No        |
+| Scope              | Function Scope | Block Scope | Block Scope |
+| Hoisted            | ✅ Yes          | ✅ Yes (TDZ) | ✅ Yes (TDZ) |
+| Modern Usage       | ❌ Avoid        | ✅ Use       | ✅ Preferred |
+
+ */
