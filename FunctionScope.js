631e2d9ee7cd7df8786dc3461881b80be85e34ee
Taterao30 <tateraog30@gmail.com>
2026-06-04 17:13:50 +0530

    Added functional scope of variables

---
+++ FunctionScope.js
@@ -0,0 +1,26 @@
+ if(true)
+{
+    var name="Taterao";
+    
+}
+console.log(name);
+
+/**
+ * 
+ * var → accessible outside the block
+let → block-scoped
+const → block-scoped
+ * 
+ * 
+ */
+
+// if (true) {
+//     let fname = "Taterao";
+// }
+
+// console.log(fname);
+// if (true) {
+//     const empName = "Taterao";
+// }
+
+// console.log(empName);
